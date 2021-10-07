ad59cf7a1b38835c926538669988689bb681f86b
Ponderix <80052935+Ponderix@users.noreply.github.com>
2021-10-07 18:39:43 +0200

    Finished retrieve and return functions
    - retrieve & return card functions
    - random two card at beginning of game function

---
+++ script/functions.js
@@ -0,0 +1,40 @@
+var functions = {
+    //random number
+    number : function(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    },
+
+    //retrieve a card from deck
+    retrieve : function(card, deck) {
+        var selected_card;
+
+        for (var i = 0; i < deck.length; i++) { //FOR EACH SUIT
+            deck[i].forEach((e, index) => {
+                if (card == e) {
+                    selected_card = e; //IF ELEMENT IS SELECTED CARD UPDATE "selected_card" BY ELEMENT
+                    deck[i].splice(index, 1, null); // REMOVE ELEMENT FROM DECK
+                }
+            });
+        }
+
+        return selected_card;
+    },
+
+    //return card to deck
+    return : function(card, deck, suits, indecies) {
+
+        for (var i = 0; i < deck.length; i++) { // FOR EACH SUIT
+            if (card.includes(suits[i])) { // IF CARD IS IN SUIT i
+
+                for (var index = 0; index < deck[i].length; index++) {
+                    if (card.includes(indecies[index][0])) { // FIND PREDETERMINED INDEX OF CARD AND RETURN CARD TO INDEX
+                        deck[i].splice(indecies[index][1], 1, card);
+                    }
+                }
+
+            }
+        }
+    },
+}
--- script/main.js
+++ script/main.js
@@ -1,31 +1,83 @@
-var deck = {
-    hearts : [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-    diamonds : [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-    clubs : [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-    spades : [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-}
+/*
+IT IS VERY IMPORTANT THAT THE CARD NAMES ARE WRITTEN ACCURATELY
+*/
 
 
-document.querySelector("btn_start").addEventListener("click", function() {
-    var player = {
-        money : 0,
-        cards : [null, null],
-        combination : null,
-        powerups : [null],
-    }
+
+// DATA //
+var deck = [
+    ["CLUBS_2", "CLUBS_3", "CLUBS_4", "CLUBS_5", "CLUBS_6", "CLUBS_7", "CLUBS_8", "CLUBS_9", "CLUBS_10", "CLUBS_J", "CLUBS_Q", "CLUBS_K", "CLUBS_A"],
+    ["DIAMONDS_2", "DIAMONDS_3", "DIAMONDS_4", "DIAMONDS_5", "DIAMONDS_6", "DIAMONDS_7", "DIAMONDS_8", "DIAMONDS_9", "DIAMONDS_10", "DIAMONDS_J", "DIAMONDS_Q", "DIAMONDS_K", "DIAMONDS_A"],
+    ["HEARTS_2", "HEARTS_3", "HEARTS_4", "HEARTS_5", "HEARTS_6", "HEARTS_7", "HEARTS_8", "HEARTS_9", "HEARTS_10", "HEARTS_J", "HEARTS_Q", "HEARTS_K", "HEARTS_A"],
+    ["SPADES_2", "SPADES_3", "SPADES_4", "SPADES_5", "SPADES_6", "SPADES_7", "SPADES_8", "SPADES_9", "SPADES_10", "SPADES_J", "SPADES_Q", "SPADES_K", "SPADES_A"],
+];
+var deck_positions = [
+    [2, 0],
+    [3, 1],
+    [4, 2],
+    [5, 3],
+    [6, 4],
+    [7, 5],
+    [8, 6],
+    [9, 7],
+    [10, 8],
+    ["J", 9],
+    ["Q", 10],
+    ["K", 11],
+    ["A", 12],
+];
+var suits = ["CLUBS", "DIAMONDS", "HEARTS", "SPADES"];
+// DATA //
+
+
+
+document.querySelector("#btn_start").addEventListener("click", function() {
+    // GAME STATE //
     var game = {
         uncovered : 0,
         current_bet : 0,
         total_bets : 0,
+        rounds_played : 0,
     }
+
+    // PARTICIPANT DATA //
     var computer = {
         money : 0,
-        cards : [null, null],
+        cards : [],
         combination : null,
-        powerups : [null],
+        powerups : null,
+    }
+    var player = {
+        money : 0,
+        cards : [],
+        combination : null,
+        powerups : null,
     }
 
-    var rounds_player = 0;
+    /* GAME START */
+    function getCards(user) {
+        var previous_card = null;
 
+        for (var i = 0; i < Infinity; i++) {
+            var random_suit = functions.number(0, 3);
+            var random_card = functions.number(0, 12);
+            var random_card = deck[random_suit][random_card];
+            var selected_card = functions.retrieve(random_card, deck);
 
+            if ((previous_card != random_card || previous_card === null) && selected_card != null) { // IF PREVIOUS CARD IS NOT THE SAME AS CURRENT CARD AND CURRENT CARD DOES EXIST
+                if (user.cards.length < 2) { // AND IF CARDS STILL HAVE TO BE SELECTED
+                    user.cards.push(selected_card); // GIVE CARD TO PLAYER
+                    previous_card = random_card;
+                } else {
+                    previous_card = random_card;
+                    break; // (IF NO MORE CARDS HAVE TO BE ASSIGNED) END LOOP
+                }
+            }
+            previous_card = random_card;
+        }
+    }
+    getCards(player);
+    getCards(computer);
+    console.log(player.cards);
+    console.log(computer.cards);
 });
