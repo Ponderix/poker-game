2acfff5be43bd4382fa739cba11ab2ada8e00de0
Ponderix <80052935+Ponderix@users.noreply.github.com>
2021-11-04 16:13:09 +0100

    Betting sequence and uncover cards works
    - moved the eventlistener functions for the betting buttons out of the function to make it easier to access
    - made the betting function start and stop bets by changing the visibility of betting buttons
    - overall works

--- script/main.js
+++ script/main.js
@@ -10,7 +10,9 @@ var deck = [
 
 
 // GAME //
+const game_start_btn = d3.select("#btn_start");
 const player_cards_div = d3.select("#player_cards");
+const computer_cards_div = d3.select("#computer_cards");
 const game_table_div = d3.select("#game_table");
 
 const player_cards_area = player_cards_div.append("div")
@@ -18,12 +20,19 @@ const player_cards_area = player_cards_div.append("div")
 const player_money_area = player_cards_div.append("div")
     .attr("id", "player_money_area");
 
+const computer_cards_area = computer_cards_div.append("div")
+    .attr("id", "computer_cards_area");
+const computer_money_area = computer_cards_div.append("div")
+    .attr("id", "computer_money_area");
+
 const game_cards_area = game_table_div.append("div")
     .attr("id", "game_cards_area");
 const game_stake_area = game_table_div.append("div")
     .attr("id", "game_stake_area");
 const game_bet_area = game_table_div.append("div")
     .attr("id", "game_bet_area");
+const game_turn_area = game_table_div.append("div")
+    .attr("id", "game_turn_area");
 
 
 const player_inputs = d3.select("#inputs").style("display", "none"); //initially hide inputs so player cant just raise during game
@@ -36,12 +45,11 @@ const turn_fold = d3.select("#btn_fold");
 document.querySelector("#btn_start").addEventListener("click", function() {
     // GAME STATE //
     var game = {
-        uncovered : 0,
-        current_bet : 0,
-        total_bets : 0,
         rounds_played : 0,
+        total_money : 0,
 
         round : {
+            uncovered : 0,
             stakes: 0 // temp to record the bets made in X round
         },
     }
@@ -68,127 +76,179 @@ document.querySelector("#btn_start").addEventListener("click", function() {
 
     //default html
     player_money_area.html(`YOUR MONEY: <span>${player.money}</span>`);
+    computer_money_area.html(`OPPONENT MONEY: <span>${computer.money}</span>`);
     game_stake_area.html(`ROUND STAKE: <span>${game.round.stakes}</span>`);
+    game_bet_area.html(`BETS: <span>0</span> | CURRENT MINBET: <span>0</span>`);
 
     //assign 2 cards to player
     player.cards = gamefunctions.getCards(deck, 2);
     computer.cards = gamefunctions.getCards(deck, 2);
     player_cards_area.html(`YOUR CARDS: <span>${player.cards[0]}</span> and <span>${player.cards[1]}</span>`);
+    computer_cards_area.html(`OPPONENT CARDS: <span>hidden</span> and <span>hidden</span>`);
+
 
     //get five cards onto playing table
     table_cards = gamefunctions.getCards(deck, 5);
-    game_cards_area.html(`<span>${table_cards[0]}</span>, COVERED, COVERED, COVERED, COVERED`);
+    console.log(table_cards);
 
     //detect which combination
 
-    //betting starts
-    (function() {
-        var round_totbet = 0; // bets made in this certain betting round
-        var round_minbet = 0; // minimum bet variable, changes
 
-        //PLAYER'S TURN ALWAYS FIRST
-        player_inputs.style("display", "block");
+    //check uncovered cards => betting sequence
+    //variables usedi n player and computer turn have to be declared here, they cannot be given as parameters as these will act as a new
+    var round_totbet = 0; // bets made in this certain betting round
+    var round_minbet = 0; // minimum bet variable, changes
+    var rounds_of_betting = 0; // how many times the rounds have continued
 
-            // PLAYER TURN //
+    var playerbet = 0;
+    var computerbet = 0;
 
-            var current_playerbet = 0;
-            var current_computerbet = 0;
+    //RAISE BET
+    turn_raise.on("keyup", function(event) {
+        if (event.code === "Enter") {
+            const input_value = Number(event.target.value);
+
+            if (isNaN(input_value) == false) { // if bet is a number
 
 
-            var rounds_of_betting = 0; // how many times the rounds have continued
+                if (player.money > input_value  && input_value >= round_minbet  && input_value > 0) { // if bet is less than player momey and if bet is larger than minimum
+                    playerbet = Number(input_value);
+                    round_totbet+=playerbet; // bet added to total round bet
+                    player.money+=(-playerbet); // money substracted from player money
 
-            //RAISE BET
-            turn_raise.on("keyup", function(event) {
-                if (event.code === "Enter") {
-                    const input_value = Number(event.target.value);
-
-                    if (isNaN(input_value) == false) { // if bet is a number
-
-                        if (input_value < player.money && input_value >= round_minbet) { // if bet is less than player momey and if bet is larger than minimum
-                            current_playerbet = Number(input_value);
-                            round_totbet+=current_playerbet; // bet added to total round bet
-                            player.money+=(-current_playerbet); // money substracted from player money
-
-                            //check if minbet should be updated
-                            if (current_playerbet > round_minbet) {
-                                round_minbet = current_playerbet;
-                            }
-                            console.log(round_minbet);
-
-                            //update html
-                            game_bet_area.html(`BETS: <span>${round_totbet}</span> | CURRENT MINBET: <span>${round_minbet}</span>`);
-                            player_money_area.html(`YOUR MONEY: <span>${player.money}</span>`);
-
-                            player_inputs.style("display", "none"); // hide inputs after turn is done
-
-                            current_playerbet = 0; // reset current playerbet
-                            computerBet() // initiate computer bet after 1 second to simulate computer thinking
-                        } else {
-                            alert("Raise exceeds moeny or is smaller than minimum bet");
-                        }
-
-                    } else {
-                        alert("Raise is not a number");
+                    //check if round_minbet should be updated
+                    if (playerbet > round_minbet) {
+                        round_minbet = playerbet;
                     }
 
-                }
-            });
-
-            //CALL BET
-            turn_call.on("click", function() {
-
-                if (round_minbet > 0 && player.money > round_minbet) {// if minimum bet exists, and player has enough money for minimum
-                    current_playerbet = round_minbet; // player bet becomes the minbet
-                    round_totbet+=current_playerbet; // bet added to total round bet
-                    player.money+=(-current_playerbet); // money substracted from player money
-
                     //update html
-                    game_bet_area.html(`BETS: <span>${round_totbet}</span> | CURRENT MINBET: <span>${round_minbet}</span>`);
+                    game_bet_area.html(`BETS: <span>${round_totbet}</span> | CURRENT MINIMUM BET: <span>${round_minbet}</span>`);
                     player_money_area.html(`YOUR MONEY: <span>${player.money}</span>`);
 
                     player_inputs.style("display", "none"); // hide inputs after turn is done
 
-                    current_playerbet = 0; // reset current playerbet
-                    computerBet() // initiate computer bet after 1 second to simulate computer thinking
+                    playerbet = 0; // reset current playerbet
+                    event.target.value = null; // reset
+
+                    //change styling and text to indicate start of computer turn
+                    game_table_div.style("background-color", "#f7a8a8");
+                    game_turn_area.html("<br></br>OPPONENT'S TURN")
+
+                    setTimeout(() =>{
+                        computer_function();
+                    }, 3000); // initiate computer bet after 3 seconds
                 } else {
-                    alert("Not enough money, or it is your turn to bet, please raise the bet");
+                    alert("Raise exceeds your money or is smaller than round minimum bet");
                 }
-            });
 
-
-            // COMPUTER TURN //
-            function computerBet() {
-                if (computer.money > round_minbet) {// if computer has enough money for minimum bet
-                    current_computerbet = round_minbet + 1; // computer bet becomes minimum bet plus 1
-                    round_totbet+=current_computerbet; // bet added to total round bet
-                    computer.money+=(-current_computerbet); // money substracted from player money
-
-                    //check if minbet should be updated
-                    if (current_computerbet > round_minbet) {
-                        round_minbet = current_computerbet;
-                    }
-
-                    //update html
-                    game_bet_area.html(`BETS: <span>${round_totbet}</span> | CURRENT MINBET: <span>${round_minbet}</span>`);
-                    player_money_area.html(`YOUR MONEY: <span>${player.money}</span>`);
-
-                    //add to rounds betted, has to be 2
-                    rounds_of_betting++;
-
-                    current_computerbet = 0; // reset current playerbet
-
-                    if (rounds_of_betting >= 2) {
-                        game_bet_area.html(`BETS: <span>N/A</span> | CURRENT MINBET: <span>N/A</span>`);
-                        game_stake_area.html(`ROUND STAKE: <span>${game.round.stakes + round_totbet}</span>`);
-                    } else {
-                        player_inputs.style("display", "block"); // re-start round
-                    }
-                } else {
-                    alert("computer folds")
-                }
+            } else {
+                alert("Raise is not a number");
             }
 
+        }
+    });
 
-    })();
+    //CALL BET
+    turn_call.on("click", function() {
+
+        if (round_minbet > 0 && player.money > round_minbet) {// if minimum bet exists, and player has enough money for minimum
+            playerbet = round_minbet; // player bet becomes the round_minbet
+            round_totbet+=playerbet; // bet added to total round bet
+            player.money+=(-playerbet); // money substracted from player money
+
+            //update html
+            game_bet_area.html(`BETS: <span>${round_totbet}</span> | CURRENT MINIMUM BET: <span>${round_minbet}</span>`);
+            player_money_area.html(`YOUR MONEY: <span>${player.money}</span>`);
+
+            player_inputs.style("display", "none"); // hide inputs after turn is done
+
+            playerbet = 0; // reset current playerbet
+
+            //change styling and text to indicate start of computer turn
+            game_table_div.style("background-color", "#f7a8a8");
+            game_turn_area.html("<br></br>OPPONENT'S TURN")
+            setTimeout(() =>{
+                computer_function();
+            }, 3000); // initiate computer bet after 3 seconds
+        } else {
+            alert("Not enough money, or it's your turn to bet, please raise the bet");
+        }
+    });
+
+
+    UNCOVER_SEQUENCE();
+
+    function UNCOVER_SEQUENCE() {
+        if (game.round.uncovered >= 0) { // if a card has already been uncovered
+            game_cards_area.selectAll("span").remove();
+
+            var uncovered = game.round.uncovered;
+            uncovered++; // one more card is uncovered
+            game.round.uncovered = uncovered;
+
+            for (var i = 0; i < uncovered; i++) { // uncover cards according to how many are already uncovered
+                game_cards_area.append("span")
+                    .html(` ${table_cards[i]},`);
+            }
+        }
+
+        game_start_btn.style("display", "none"); // hide start btn so it cannot be abused
+
+        //PLAYER'S TURN ALWAYS FIRST
+        player_inputs.style("display", "block");
+        game_table_div.style("background-color", "lightgreen");
+        game_turn_area.html("<br></br>YOUR TURN")
+
+    };
+
+    function computer_function() {
+
+        var computerbet = 0;
+
+        if (computer.money > round_minbet) {// if computer has enough money for minimum bet
+            computerbet = round_minbet + 1; // computer bet becomes minimum bet plus 1
+            round_totbet+=computerbet; // bet added to total round bet
+            computer.money+=(-computerbet); // money substracted from player money
+
+            //check if round_minbet should be updated
+            if (computerbet > round_minbet) {
+                round_minbet = computerbet;
+            }
+
+            //update html
+            game_bet_area.html(`BETS: <span>${round_totbet}</span> | CURRENT MINIMUM BET: <span>${round_minbet}</span>`);
+            computer_money_area.html(`YOUR MONEY: <span>${computer.money}</span>`);
+
+            //add to rounds betted, has to be 2
+            rounds_of_betting++;
+
+            if (rounds_of_betting >= 2) {
+
+                game_bet_area.html(`BETS: <span>0</span> | CURRENT MINIMUM BET: <span>0</span>`);
+                game_stake_area.html(`ROUND STAKE: <span>${game.round.stakes + round_totbet}</span>`);
+                game_table_div.style("background-color", "lightblue");
+                game_turn_area.html("<br></br>CARD IS BEING UNCOVERED");
+
+                //resetting variables
+                rounds_of_betting = 0;
+                round_minbet = 0;
+                round_totbet = 0;
+
+                setTimeout(() =>{
+                    UNCOVER_SEQUENCE();
+                }, 3000); // uncover next card
+            } else {
+                rounds_of_betting++;
+
+                computerbet = 0; // reset current computerbet
+
+                player_inputs.style("display", "block"); // second turn of betting
+                game_turn_area.html("<br></br>YOUR TURN")
+                game_table_div.style("background-color", "lightgreen");
+            }
+        } else {
+            alert("computer folds")
+        }
+    }
 
 });
