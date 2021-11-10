0c57da825644d201c8d6be45468a58cd102bb559
Ponderix <80052935+Ponderix@users.noreply.github.com>
2021-11-10 12:21:26 +0100

    Finished game system
    - finished basic round system
    - finished card comparison system
    - only 1 round of the game as of yet
    - STILL need to add visuals and styling
    - STILL need to computer AI

--- script/gamefunctions.js
+++ script/gamefunctions.js
@@ -17,7 +17,7 @@ var gamefunctions = {
             ["K", 11],
             ["A", 12],
         ];
-        var suits = ["CLUBS", "DIAMONDS", "HEARTS", "SPADES"];
+        var suits = ["clubs", "diamonds", "hearts", "spades"];
 
         var previous_card = null;// CHECK IF TWO CARDS SAME
 
@@ -41,5 +41,232 @@ var gamefunctions = {
         }
 
         return output;
+    },
+
+    detectCombination: function(usercards, tablecards) {
+        var cards = usercards.concat(tablecards);
+        var hand = {
+            type: null, // type of hand pair, flush etc
+            value: null, // how much value this type has, e.g flush is higher than pair
+            cards: [], // the cards which make up the certain type/combination, e.g pair could be [spades_J, clubs_J]
+        }
+
+        var suits = [
+            ["clubs"],
+            ["diamonds"],
+            ["hearts"],
+            ["spades"],
+        ]
+
+        var ranks = [["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"], ["9"], ["10"], ["J"], ["Q"], ["K"], ["A"]];
+
+        for (var i = 0; i < cards.length; i++) { // order by suit type
+
+            for (var index = 0; index < suits.length; index++) {
+                if (cards[i].includes(suits[index][0])) {
+                    suits[index].push(cards[i]);
+                }
+            }
+
+        }
+
+        for (var i = 0; i < cards.length; i++) {
+
+            for (var index = 0; index < ranks.length; index++) {
+                if (cards[i].includes(ranks[index][0])) {
+                    ranks[index].push(cards[i]);
+                }
+            }
+
+        }
+
+        /* COMBINATION CHECKS
+            * straight and straight flush are not yet included since they require complex code
+            * it is important that the filtering functions are ordered from least valuable hand to highest hand so that if
+              for example both a pair and a triple get detected (which will always be the case), the triple will be selected because it
+              was filtered for last
+        */
+
+        //check for high card
+        (function() {
+            var index = 0; // index of highest card which has been filtered as of yet
+
+            for (var i = 0; i < ranks.length; i++) {
+                if (ranks[i].length == 2) {
+                    index = i;
+                }
+            }
+
+            hand.type = "High Card";
+            hand.cards = [[ranks[index][1], index]];
+            hand.value = 0;
+        })();
+
+
+        //check for pair
+        for (var i = 0; i < ranks.length; i++) {
+            if (ranks[i].length == 3) {
+                hand.type = "Pair";
+                hand.cards = [[ranks[i][1], i], [ranks[i][2], i]];
+                hand.value = 1;
+            }
+        }
+
+        //check for two pair
+        (function() {
+            var pairs = 0;
+            var pair1_index = 0;
+            var pair2_index = 0;
+
+            for (var i = 0; i < ranks.length; i++) {
+
+                if (ranks[i].length == 3) {
+                    if (pair1_index == 0) {
+                        pair1_index = i;
+                    } else {
+                        if (pair2_index ==0) {
+                            pair2_index = i;
+                        }
+                    }
+
+                    pairs++;
+                }
+
+                if (pairs >= 2) {
+                    hand.type = "Two Pair";
+                    hand.cards = [
+                        [ranks[pair1_index][1], pair1_index],
+                        [ranks[pair1_index][2], pair1_index],
+                        [ranks[pair2_index][1], pair2_index],
+                        [ranks[pair2_index][2], pair2_index]
+                    ];
+                    hand.value = 2;
+                }
+            }
+        })();
+
+        //check for three of a kind
+        for (var i = 0; i < ranks.length; i++) {
+            if (ranks[i].length == 4) {
+                hand.type = "Three of a Kind";
+                hand.cards = [[ranks[i][1], i], [ranks[i][2], i], [ranks[i][3], i]];
+                hand.value = 3;
+            }
+        }
+
+        //check for straight (?)
+
+
+        //check for flush
+        (function() {
+            for (var i = 0; i < suits.length; i++) {
+                if (suits[i].length >= 6) {
+                    hand.type = "Flush";
+                    hand.cards = [
+                        [suits[i][1]], [suits[i][2]],
+                        [suits[i][3]], [suits[i][4]],
+                        [suits[i][5]]
+                    ];
+                    hand.value = 5;
+                }
+            }
+
+            //assign each card the its rank based upon which position it has in the ranks array
+            for (var i = 0; i < ranks.length; i++) {
+                for (var index = 0; index < hand.cards.length; index++) {
+
+                    ranks[i].filter((e) =>{
+                        if (e.includes(hand.cards[index])) {
+                            hand.cards[index].push(i);
+                        }
+                    });
+                    /* meaning: for every rank, filter for a card which makes up the flush, and push the rank of this card to the card in the card array*/
+
+                }
+            }
+        })();
+
+        //check for full house
+        (function() {
+            var pairs = 0;
+            var pair_index = 0;
+
+            var triplets = 0;
+            var triplets_index = 0;
+
+            for (var i = 0; i < ranks.length; i++) {
+
+                if (ranks[i].length == 3) {
+                    pair_index = i;
+                    pairs++;
+                }
+                if (ranks[i].length == 4) {
+                    triplets_index = i;
+                    triplets++;
+                }
+
+                if (pairs > 0 && triplets > 0) {
+                    hand.type = "Full House";
+                    hand.cards = [
+                        [ranks[triplets_index][1], triplets_index],
+                        [ranks[triplets_index][2], triplets_index],
+                        [ranks[triplets_index][3], triplets_index],
+                        [ranks[pair_index][1], pair_index],
+                        [ranks[pair_index][2], pair_index]
+                    ];
+                    hand.value = 6;
+                }
+            }
+        })();
+
+        //check for four of a kind
+        for (var i = 0; i < ranks.length; i++) {
+            if (ranks[i].length == 5) {
+                hand.type = "Four of a Kind";
+                hand.cards = [[ranks[i][1], i], [ranks[i][2], i], [ranks[i][3], i], [ranks[i][4], i]];
+                hand.value = 7;
+            }
+        }
+
+        //check for straight flush (?)
+
+
+        //check for royal flush
+        (function() {
+            for (var i = 0; i < suits.length; i++) {
+                if (suits[i].length > 1) {
+
+                    if (suits[i].includes(suits[i][0] + "_10")
+                        && suits[i].includes(suits[i][0] + "_J")
+                        && suits[i].includes(suits[i][0] + "_Q")
+                        && suits[i].includes(suits[i][0] + "_K")
+                        && suits[i].includes(suits[i][0] + "_A")
+                    ) {
+                        hand.type = "Royal Flush";
+                        hand.cards = [[suits[i][1]], [suits[i][2]], [suits[i][3]], [suits[i][4]], [suits[i][5]]];
+                        hand.value = 9;
+                    }
+
+                }
+            }
+            //assign each card the its rank based upon which position it has in the ranks array
+            for (var i = 0; i < ranks.length; i++) {
+                for (var index = 0; index < hand.cards.length; index++) {
+
+                    ranks[i].filter((e) =>{
+                        if (e.includes(hand.cards[index])) {
+                            hand.cards[index].push(i);
+                        }
+                    });
+
+                }
+            }
+        })();
+
+        //sort in descending order to make it easier to find highest card in case of tie
+        hand.cards.sort((a, b) =>{
+            return b[1] - a[1];
+        });
+        return hand;
     },
 }
--- script/main.js
+++ script/main.js
@@ -1,29 +1,32 @@
 // DATA //
 
 var deck = [
-    ["CLUBS_2", "CLUBS_3", "CLUBS_4", "CLUBS_5", "CLUBS_6", "CLUBS_7", "CLUBS_8", "CLUBS_9", "CLUBS_10", "CLUBS_J", "CLUBS_Q", "CLUBS_K", "CLUBS_A"],
-    ["DIAMONDS_2", "DIAMONDS_3", "DIAMONDS_4", "DIAMONDS_5", "DIAMONDS_6", "DIAMONDS_7", "DIAMONDS_8", "DIAMONDS_9", "DIAMONDS_10", "DIAMONDS_J", "DIAMONDS_Q", "DIAMONDS_K", "DIAMONDS_A"],
-    ["HEARTS_2", "HEARTS_3", "HEARTS_4", "HEARTS_5", "HEARTS_6", "HEARTS_7", "HEARTS_8", "HEARTS_9", "HEARTS_10", "HEARTS_J", "HEARTS_Q", "HEARTS_K", "HEARTS_A"],
-    ["SPADES_2", "SPADES_3", "SPADES_4", "SPADES_5", "SPADES_6", "SPADES_7", "SPADES_8", "SPADES_9", "SPADES_10", "SPADES_J", "SPADES_Q", "SPADES_K", "SPADES_A"],
+    ["clubs_2", "clubs_3", "clubs_4", "clubs_5", "clubs_6", "clubs_7", "clubs_8", "clubs_9", "clubs_10", "clubs_J", "clubs_Q", "clubs_K", "clubs_A"],
+    ["diamonds_2", "diamonds_3", "diamonds_4", "diamonds_5", "diamonds_6", "diamonds_7", "diamonds_8", "diamonds_9", "diamonds_10", "diamonds_J", "diamonds_Q", "diamonds_K", "diamonds_A"],
+    ["hearts_2", "hearts_3", "hearts_4", "hearts_5", "hearts_6", "hearts_7", "hearts_8", "hearts_9", "hearts_10", "hearts_J", "hearts_Q", "hearts_K", "hearts_A"],
+    ["spades_2", "spades_3", "spades_4", "spades_5", "spades_6", "spades_7", "spades_8", "spades_9", "spades_10", "spades_J", "spades_Q", "spades_K", "spades_A"],
 ];
-
-
 
 // GAME //
 const game_start_btn = d3.select("#btn_start");
 const player_cards_div = d3.select("#player_cards");
 const computer_cards_div = d3.select("#computer_cards");
 const game_table_div = d3.select("#game_table");
+const game_round_btn = d3.select("#btn_next_round").style("display", "none")
 
 const player_cards_area = player_cards_div.append("div")
     .attr("id", "player_cards_area");
 const player_money_area = player_cards_div.append("div")
     .attr("id", "player_money_area");
+const player_hand_area = player_cards_div.append("div")
+    .attr("id", "player_hand_area");
 
 const computer_cards_area = computer_cards_div.append("div")
     .attr("id", "computer_cards_area");
 const computer_money_area = computer_cards_div.append("div")
     .attr("id", "computer_money_area");
+const computer_hand_area = computer_cards_div.append("div")
+    .attr("id", "computer_hand_area");
 
 const game_cards_area = game_table_div.append("div")
     .attr("id", "game_cards_area");
@@ -62,7 +65,7 @@ document.querySelector("#btn_start").addEventListener("click", function() {
         powerups : null,
     }
     var computer = {
-        money :1000,
+        money : 1000,
         cards : null,
         combination : null,
         powerups : null,
@@ -89,10 +92,6 @@ document.querySelector("#btn_start").addEventListener("click", function() {
 
     //get five cards onto playing table
     table_cards = gamefunctions.getCards(deck, 5);
-    console.log(table_cards);
-
-    //detect which combination
-
 
     //check uncovered cards => betting sequence
     //variables usedi n player and computer turn have to be declared here, they cannot be given as parameters as these will act as a new
@@ -175,11 +174,34 @@ document.querySelector("#btn_start").addEventListener("click", function() {
         }
     });
 
+    //FOLD
+    turn_fold.on("click", function () {
+        game.round.stakes+=round_totbet; //in case fold happens during betting
+        computer.money+=game.round.stakes; //computer gets all the money
+
+        game_bet_area.html("");
+        game_stake_area.html("")
+        game_table_div.style("background-color", "lightblue");
+        game_turn_area.html(`<br></br>OPPONENT WINS $${game.round.stakes}`);
+        computer_money_area.html(`OPPONENT MONEY: <span>${computer.money}</span>`);
+
+        player_inputs.style("display", "none");
+        game_round_btn.style("display", "block");
+
+        alert("Player FOLDS");
+    });
+
+    //FOLD
+    game_round_btn.on("click", function () {
+         location.reload();
+    });
+
 
     UNCOVER_SEQUENCE();
 
     function UNCOVER_SEQUENCE() {
-        if (game.round.uncovered >= 0) { // if a card has already been uncovered
+
+        if (game.round.uncovered < 5) {
             game_cards_area.selectAll("span").remove();
 
             var uncovered = game.round.uncovered;
@@ -190,6 +212,95 @@ document.querySelector("#btn_start").addEventListener("click", function() {
                 game_cards_area.append("span")
                     .html(` ${table_cards[i]},`);
             }
+
+            if (game.round.uncovered == 5) { //if last card is being uncovered
+                var player_hand = gamefunctions.detectCombination(getParams(player)[0], getParams(player)[1]);
+                var computer_hand = gamefunctions.detectCombination(getParams(computer)[0], getParams(computer)[1]);
+
+                setTimeout(() =>{ // as final card is uncovered refresh hand
+                    player_hand_area.html(`<br></br>YOUR HAND: <span>${player_hand.type}</span>`);
+
+                    game_table_div.style("background-color", "lightblue");
+                    game_turn_area.html(`<br></br>ALL CARDS ARE UNCOVERED`);
+                    player_inputs.style("display", "none");
+                    game_stake_area.html(`ROUND STAKE: <span>${game.round.stakes}</span>`);
+                    game_bet_area.html("");
+
+                    setTimeout(() =>{ // then show opponents cards
+                        game_turn_area.html(`<br></br>OPPONENTS CARDS HAVE BEEN REVEALED<br>calculating winner...`);
+
+                        //reveal computer's cards
+                        computer_cards_area.html(`OPPONENT CARDS: <span>${computer.cards[0]}</span> and <span>${computer.cards[1]}</span>`);
+                        computer_hand_area.html(`<br></br>OPPONENT'S HAND: <span>${computer_hand.type}</span>`);
+
+
+                        setTimeout(() =>{// finally compare player and computer hand
+                            decide_winner();
+                        }, 4000);
+                    }, 3000)
+
+                }, 0);
+
+                function decide_winner() {
+                    if (player_hand.value > computer_hand.value) {
+                        player_win();
+                        alert("You have WON!")
+                    }
+                    if (computer_hand.value > player_hand.value) {
+                        computer_win();
+                        alert("You have LOST!")
+                    }
+
+                    if (player_hand.value === computer_hand.value) {
+
+                        if (player_hand.cards[0][1] > computer_hand.cards[0][1]) {
+                            player_win();
+                            alert(`You have WON!\n(the ${player_hand.type} with the highest rank decides the winner)`);
+                        } else {
+
+                            if (computer_hand.cards[0][1] > player_hand.cards[0][1]) {
+                                computer_win();
+                                alert(`You have LOST!\n(the ${player_hand.type} with the highest rank decided winner)`);
+                            } else {
+                                computer.money+=(0.5*game.round.stakes); //computer gets all the money
+                                player.money+=(0.5*game.round.stakes); //computer gets all the money
+
+                                game_turn_area.html(`<br></br>YOU HAVE BOTH WON $${0.5*game.round.stakes}`);
+                                computer_money_area.html(`OPPONENT MONEY: <span>${computer.money}</span>`);
+                                computer_money_area.html(`OPPONENT MONEY: <span>${player.money}</span>`);
+                                game_stake_area.html("")
+
+
+                                game_round_btn.style("display", "block");
+                                alert(`You have TIED, because both players have the same hand with the same rank!`)
+                            }
+
+                        }
+
+                    }
+                }
+
+                function computer_win() {
+                    computer.money+=game.round.stakes; //computer gets all the money
+
+                    game_turn_area.html(`<br></br>OPPONENT WINS $${game.round.stakes}`);
+                    computer_money_area.html(`OPPONENT MONEY: <span>${computer.money}</span>`);
+                    game_stake_area.html("")
+
+
+                    game_round_btn.style("display", "block");
+                }
+                function player_win() {
+                    player.money+=game.round.stakes; //computer gets all the money
+
+                    game_turn_area.html(`<br></br>YOU WIN $${game.round.stakes}`);
+                    player_money_area.html(`YOUR MONEY: <span>${player.money}</span>`);
+                    game_stake_area.html("")
+
+
+                    game_round_btn.style("display", "block");
+                }
+            }
         }
 
         game_start_btn.style("display", "none"); // hide start btn so it cannot be abused
@@ -199,7 +310,26 @@ document.querySelector("#btn_start").addEventListener("click", function() {
         game_table_div.style("background-color", "lightgreen");
         game_turn_area.html("<br></br>YOUR TURN")
 
+        //DETECT CARD COMBINATION
+        var player_hand = gamefunctions.detectCombination(getParams(player)[0], getParams(player)[1]);
+        var computer_hand = gamefunctions.detectCombination(getParams(computer)[0], getParams(computer)[1]);
+
+        player_hand_area.html(`<br></br>YOUR HAND: <span>${player_hand.type}</span>`);
+        computer_hand_area.html(`<br></br>OPPONENT'S HAND: <span>hidden</span>`);
+
+        function getParams(user) {
+            var param1 = user.cards;
+            var param2 = [];
+
+            for (var i = 0; i < game.round.uncovered; i++) { // select only cards which are uncovered
+                param2.push(table_cards[i]);
+            }
+
+            return [param1, param2];
+        }
     };
+
+
 
     function computer_function() {
 
@@ -217,17 +347,27 @@ document.querySelector("#btn_start").addEventListener("click", function() {
 
             //update html
             game_bet_area.html(`BETS: <span>${round_totbet}</span> | CURRENT MINIMUM BET: <span>${round_minbet}</span>`);
-            computer_money_area.html(`YOUR MONEY: <span>${computer.money}</span>`);
+            computer_money_area.html(`OPPONENT MONEY: <span>${computer.money}</span>`);
 
             //add to rounds betted, has to be 2
             rounds_of_betting++;
 
             if (rounds_of_betting >= 2) {
+                game.round.stakes+=round_totbet;
 
                 game_bet_area.html(`BETS: <span>0</span> | CURRENT MINIMUM BET: <span>0</span>`);
-                game_stake_area.html(`ROUND STAKE: <span>${game.round.stakes + round_totbet}</span>`);
+                game_stake_area.html(`ROUND STAKE: <span>${game.round.stakes}</span>`);
                 game_table_div.style("background-color", "lightblue");
-                game_turn_area.html("<br></br>CARD IS BEING UNCOVERED");
+
+                if (game.round.uncovered == 4) {
+                    game_table_div.style("background-color", "lightblue");
+                    game_turn_area.html(`<br></br>FINAL CARD IS BEING UNCOVERED`);
+                    player_inputs.style("display", "none");
+                    game_stake_area.html(`ROUND STAKE: <span>${game.round.stakes}</span>`);
+                    game_bet_area.html("");
+                } else {
+                    game_turn_area.html("<br></br>CARD IS BEING UNCOVERED");
+                }
 
                 //resetting variables
                 rounds_of_betting = 0;
